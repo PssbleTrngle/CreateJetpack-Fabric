14118d6b60956650e62119b34a3d63460d20e04b
Niklas Widmann <nik@somethingcatchy.net>
2024-11-20 11:20:05 +0100

    remove durability from jetpacks

--- src/main/kotlin/com/possible_triangle/create_jetpack/Content.kt
+++ src/main/kotlin/com/possible_triangle/create_jetpack/Content.kt
@@ -33,6 +33,9 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
 import net.minecraft.client.renderer.RenderType
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider
 import net.minecraft.core.Registry
+import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.tags.TagKey
 import net.minecraft.world.item.ArmorMaterials
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.Rarity
@@ -59,6 +62,8 @@ object Content {
             ItemDescription.Modifier(it, TooltipHelper.Palette.STANDARD_CREATE)
                 .andThen(TooltipModifier.mapNull(KineticStats.create(it)))
         }
+
+    private val CHESTPLATES = TagKey.create(Registries.ITEM, ResourceLocation("c", "chestplates"))
 
     val COPY_NBT_MECHANICAL_CRAFTING_SERIALIZER = REGISTRATE
         .simple(
@@ -141,6 +146,8 @@ object Content {
                 .texture("0", "block/${c.name}")
         }
         tag(AllItemTags.PRESSURIZED_AIR_SOURCES.tag)
+        properties { it.durability(-1) } // fabric: Item#canBeDepleted() isn't enough to disable durability, so we need to set its maxDamage to -1 as well
+        tag(CHESTPLATES)
 
         owner.addRawLang("item.${REGISTRATE.modid}.${name}.tooltip", "")
         owner.addRawLang("item.${REGISTRATE.modid}.${name}.tooltip.summary", "Allows levitation using pressurized air")
