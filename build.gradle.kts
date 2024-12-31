9f300f1a6166b8e62cb5f7e2d5ca7be3738214b7
Niklas Widmann <nik@somethingcatchy.net>
2024-12-31 12:28:10 +0100

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ val trinkets_version: String by extra
 val cca_version: String by extra
 
 plugins {
-    id("com.possible-triangle.gradle") version ("0.1.4")
+    id("com.possible-triangle.gradle") version ("0.2.5")
 }
 
 withKotlin()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         gradlePluginPortal()
         maven { url = uri("https://maven.minecraftforge.net/") }
         maven { url = uri("https://repo.spongepowered.org/repository/maven-public/") }
+        maven { url = uri("https://maven.neoforged.net/releases/") }
     }
 }
 
